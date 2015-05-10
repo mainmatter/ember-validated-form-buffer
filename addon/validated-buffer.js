343d73b1687aa46c44715e5b03f30992e282f5e3
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-05-10 14:19:06 +0200

    require the object to have a container property

--- addon/validated-buffer.js
+++ addon/validated-buffer.js
@@ -1,11 +1,7 @@
 import EmberValidations from 'ember-validations';
 import BufferedProxy from 'ember-buffered-proxy/proxy';
 
-export default function validatedBuffer(object, classBody, container) {
-  if (container) {
-    object.set('container', container);
-  }
-
+export default function validatedBuffer(object, classBody) {
   var Buffer = BufferedProxy.extend(EmberValidations.Mixin, classBody);
 
   return Buffer.create({
