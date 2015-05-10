912c1d912e1d35bb571181b09bf030964d028126
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-05-10 15:57:35 +0200

    added formObject.apiErrors

--- addon/form-object.js
+++ addon/form-object.js
@@ -3,12 +3,6 @@ import validatedBuffer from './validated-buffer';
 
 export default function formObject(model, impl = {}) {
   return Ember.computed(model, function() {
-    //This makes sure that model errors are displayed via the buffer correctly but only until the
-    //property is modified again.
-    Ember.keys(impl.validations || {}).forEach(function(key) {
-      impl.validations[key]['api-errors'] = true;
-    });
-
     return validatedBuffer(this.get(model), impl);
   });
 }
--- addon/validated-buffer.js
+++ addon/validated-buffer.js
@@ -2,7 +2,9 @@ import EmberValidations from 'ember-validations';
 import BufferedProxy from 'ember-buffered-proxy/proxy';
 
 export default function validatedBuffer(object, classBody) {
-  var Buffer = BufferedProxy.extend(EmberValidations.Mixin, classBody);
+  var Buffer = BufferedProxy.extend(EmberValidations.Mixin, classBody, {
+    apiErrors: Ember.computed.oneWay('content.errors')
+  });
 
   return Buffer.create({
     content: object
