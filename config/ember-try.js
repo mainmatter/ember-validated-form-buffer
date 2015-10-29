bccc298f14ef2ab9e1968e07eccd20fdbd22bd41
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-10-29 20:14:03 +0100

    setup ember-try and travis

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,28 +7,45 @@ module.exports = {
     {
       name: 'ember-release',
       dependencies: {
-        'ember': 'components/ember#release'
+        'ember': 'components/ember#release',
+        'ember-data': 'components/ember-data#release'
       },
       resolutions: {
-        'ember': 'release'
+        'ember': 'release',
+        'ember-data': 'beta'
+      }
+    },
+    {
+      name: 'ember-earliest',
+      dependencies: {
+        'ember': 'components/ember#1.12',
+        'ember-data': 'components/ember-data#1.13'
+      },
+      resolutions: {
+        'ember': '1.12',
+        'ember-data': '1.13'
       }
     },
     {
       name: 'ember-beta',
       dependencies: {
-        'ember': 'components/ember#beta'
+        'ember': 'components/ember#beta',
+        'ember-data': 'components/ember-data#beta'
       },
       resolutions: {
-        'ember': 'beta'
+        'ember': 'beta',
+        'ember-data': 'beta'
       }
     },
     {
       name: 'ember-canary',
       dependencies: {
-        'ember': 'components/ember#canary'
+        'ember': 'components/ember#canary',
+        'ember-data': 'components/ember-data#canary'
       },
       resolutions: {
-        'ember': 'canary'
+        'ember': 'canary',
+        'ember-data': 'canary'
       }
     }
   ]
